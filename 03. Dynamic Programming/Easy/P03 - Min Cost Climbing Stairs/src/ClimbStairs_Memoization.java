55b0c74eaf0beac1460927b27b37b9b4854f6f06
mirchanv <137283854+mirchanv@users.noreply.github.com>
2023-07-17 13:27:34 +0200

    Add files via upload

--- 03. Dynamic Programming/Easy/P03 - Min Cost Climbing Stairs/src/ClimbStairs_Memoization.java
+++ 03. Dynamic Programming/Easy/P03 - Min Cost Climbing Stairs/src/ClimbStairs_Memoization.java
@@ -3,22 +3,24 @@ import java.util.Arrays;
 public class ClimbStairs_Memoization {
 
     public static int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
-        Arrays.fill(dp, -1);
+            int[] dp = new int[cost.length];
+            Arrays.fill(dp, -1);
 
-        return Math.min(helper(cost, 0, dp) , helper(cost, 1, dp));
-    }
+            return Math.min(helper(cost, 0, dp) , helper(cost, 1, dp));
+        }
 
-    private static int helper(int[] cost, int index, int[] dp) {
-        if (index >= cost.length)
-            return 0;
+        private static int helper(int[] cost, int index, int[] dp) {
+            if (index == cost.length)
+                return 0;
 
-        if (dp[index] != -1)
-            return dp[index];
+            if (dp[index] != -1)
+                return dp[index];
 
-        int oneStep = cost[index] + helper(cost, index + 1, dp);
-        int twoStep = cost[index] + helper(cost, index + 2, dp);
+            int oneStep = cost[index] + helper(cost, index + 1, dp);
 
-        return dp[index] = Math.min(oneStep, twoStep);
-    }
+            if ((index + 2) <= cost.length)
+                return dp[index] = Math.min(oneStep, cost[index] + helper(cost, index + 2, dp));
+
+            return dp[index] = oneStep;
+        }
 }
