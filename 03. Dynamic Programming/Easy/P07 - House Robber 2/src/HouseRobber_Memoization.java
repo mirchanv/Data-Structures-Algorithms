167903bed07b99b856ca90e7e6a798ca42f734be
mirchanv <137283854+mirchanv@users.noreply.github.com>
2023-08-03 16:03:35 +0200

    Add files via upload

--- 03. Dynamic Programming/Easy/P07 - House Robber 2/src/HouseRobber_Memoization.java
+++ 03. Dynamic Programming/Easy/P07 - House Robber 2/src/HouseRobber_Memoization.java
@@ -3,11 +3,16 @@ import java.util.Arrays;
 public class HouseRobber_Memoization {
 
     public static int rob(int[] money) {
-        int[] dp = new int[money.length];
-        Arrays.fill(dp, -1);
+        if (money.length == 1)
+            return money[0];
 
-        int ans1 = helper(money, 0, money.length-1, dp);
-        int ans2 = helper(money, 1, money.length, dp);
+        int[] dp_temp1 = new int[money.length];
+        int[] dp_temp2 = new int[money.length];
+        Arrays.fill(dp_temp1, -1);
+        Arrays.fill(dp_temp2, -1);
+
+        int ans1 = helper(money, 0, money.length-1, dp_temp1);
+        int ans2 = helper(money, 1, money.length, dp_temp2);
         return Math.max(ans1, ans2);
     }
 
@@ -16,7 +21,7 @@ public class HouseRobber_Memoization {
 
         if (dp[startIndex] != -1) return dp[startIndex];
 
-        int pick = arr[startIndex] + helper(arr, startIndex + 2, endIndex, dp);
+        int pick = arr[startIndex]+ helper(arr, startIndex + 2, endIndex, dp);
         int notPick = helper(arr, startIndex + 1, endIndex, dp);
 
         return dp[startIndex] = Math.max(pick, notPick);
--- 03. Dynamic Programming/Easy/P07 - House Robber 2/src/Test.java
+++ 03. Dynamic Programming/Easy/P07 - House Robber 2/src/Test.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        int[] valueInHouse = {2,3,2};
+        int[] valueInHouse = {1, 2, 3};
         System.out.println("valueInHouse = " + Arrays.toString(valueInHouse));
         System.out.println("Maximum sum we can rob = " + HouseRobber_Recursion.rob(valueInHouse));
 
