3256bc5448721b11f36dc6c88d888f8378109b89
mirchanv <137283854+mirchanv@users.noreply.github.com>
2023-12-06 19:05:40 +0100

    Delete Trees/Easy/P14 - Same Tree directory

--- Trees/Easy/P14 - Same Tree/src/Solution.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-
-    public static boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        else if (p == null || q == null || p.val != q.val) return false;
-
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    }
-}
